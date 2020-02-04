c0d90a40f2d675a6bf8f95351bf334c498fb1acb
Fanny <fanny.f.nilsson@hyperisland.se>
2020-02-04 15:49:22 +0100

    creating a new branch

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import Button from './components/Button';
 import Train from './components/Train';
+import Map from './components/Map';
 
 class App extends React.Component {
-  state = {};
+    state = {};
 
-  getTrains = async e => {
-    e.preventDefault();
-    const api_call = await fetch('http://localhost:8081');
-    const data = await api_call.json();
-    console.log(data);
+    getTrains = async e => {
+        e.preventDefault();
+        const api_call = await fetch('http://localhost:8081');
+        const data = await api_call.json();
+        console.log(data);
 
-    this.setState({
-      trains: data.trains.slice(0, 15)
-    }
-    );
-    console.log(this.state);
-
-  }
-
-  render() {
-    return (
-      <div>
-        <Button getTrains={this.getTrains} />
-        {this.state.trains ?
-          this.state.trains.map((train, index) => {
-            return <Train
-              key={index}
-              station={train.station.station}
-              train={train.train}
-              start={train.trip.from}
-              destination={train.trip.to} />
-          }) : ''
+        this.setState({
+            trains: data.trains.slice(0, 15)
         }
-      </div>
-    )
-  }
+        );
+        console.log(this.state);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Button getTrains={this.getTrains} />
+                {this.state.trains ?
+                    this.state.trains.map((train, index) => {
+                        return <Train
+                            key={index}
+                            station={train.station.station}
+                            train={train.train}
+                            start={train.trip.from}
+                            destination={train.trip.to} />
+                    }) : ''
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
